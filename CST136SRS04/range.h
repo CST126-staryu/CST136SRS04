22f39c5a0c2fb1882087aa3fb5ac24a452b820c0
CST126-staryu <alexandra.sanderson@oit.edu>
2018-06-02 10:34:25 -0700

    First
      ...

---
+++ CST136SRS04/range.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+template<typename T, T min, T max>
+class Range // You should re-code and simplify your SRS01 RangeInt
+{
+private:
+	using value_type = T;
+
+	value_type value_;
+
+public:
+	Range(const value_type value);
+};
+
--- REPORT.md
+++ REPORT.md
@@ -1,1 +1,9 @@
+# CST136SRS04
+Island Hopping
 
+Student Name: Sasha Sanderson   
+
+Issues:
+
+
+Notes:
